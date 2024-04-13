0e0a7c14a791a767de28290b30215eb1b13e74c2
Evan-Wildenhain <66094938+Evan-Wildenhain@users.noreply.github.com>
2024-04-13 11:09:28 -0400

    rmoved updated dependecies

--- pythons/app.py
+++ pythons/app.py
@@ -7,48 +7,20 @@ import json
 from model import *
 import torch
 from g2p_en import G2p
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 from faster_whisper import WhisperModel
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model_size = "medium"
 model_whisper = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 
 app = Flask(__name__)
 CORS(app)
+print(DEVICE)
 regexp = re.compile(r'https://www.youtube.com/watch\?v=')
 model = torch.load('model.pth')
 model = model.to(DEVICE)
 g2p = G2p()
 
-def getOutdatedPackages():
-    outdated_packages = []
-    output = subprocess.check_output(["pip", "list", "--outdated", "--format", "json"])
-    outdated_packages_list = json.loads(output)
-
-    for package in outdated_packages_list:
-        outdated_packages.append(package['name'])
-
-    return outdated_packages
-
-def updatePackage(package_name):
-    try:
-        output = subprocess.check_output(["conda", "install", "--upgrade", package_name], timeout=60)  # 60 seconds timeout
-        print(f"Successfully updated {package_name}")
-    except subprocess.TimeoutExpired:
-        print(f"Timed out while trying to update {package_name}.")
-    except Exception as ex:
-        print(f"Error occurred while trying to update {package_name}. Error: {str(ex)}")
-
-
-def updatePackages():
-    with open('requirements.txt', 'r') as f:
-        required_packages = f.read().splitlines()
-
-    outdated_packages = getOutdatedPackages()
-
-    for package in outdated_packages:
-        if package in required_packages:
-            updatePackage(package)
 
 
 @app.route('/', methods=['POST'])
@@ -64,6 +36,4 @@ def handle_data():
                  'similar_phonemes': similar_phonemes}, 200
 
 if __name__ == '__main__':
-    with app.app_context():
-        updatePackages()
     app.run(port=3000)
